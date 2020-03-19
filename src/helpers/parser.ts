276c0df678bb8d35f809cace0f02b1f29887de76
2CHEVSKII <prsroman3@gmail.com>
2020-03-20 00:19:47 +0300

    Parser v2: simple, clean and beautiful

--- src/helpers/parser.ts
+++ src/helpers/parser.ts
@@ -1,34 +1,67 @@
-function splitDottedPath(dottedPath: string): string[] {
-    const escapeChar = '\\';
-    const dotChar = '.';
-    const propParts = new Array<string>();
+// function splitDottedPath(dottedPath: string): string[] {
+//     const escapeChar = '\\';
+//     const dotChar = '.';
+//     const propParts = new Array<string>();
+//     let currentPart = '';
+//     const savePart = () => {
+//         if (currentPart.length > 0 && ![...currentPart].every(char => char === ' ')) {
+//             propParts.push(currentPart);
+//             currentPart = '';
+//         }
+//     };
+//     for (let i = 0; i < dottedPath.length; i++) {
+//         const currentChar = dottedPath[i];
+//         const nextChar = dottedPath[i + 1];
+//         if (currentChar === escapeChar && (nextChar === escapeChar || nextChar === dotChar)) {
+//             currentPart += nextChar;
+//             i += 1;
+//             continue;
+//         }
+//         if (!(currentChar === dotChar)) {
+//             currentPart += currentChar;
+//             if (i !== dottedPath.length - 1) {
+//                 continue;
+//             }
+//         }
+//         savePart();
+//     }
+//     for (let i = 0; i < propParts.length; i++) {
+//         propParts[i] = propParts[i].replace('\\\\', '\\');
+//     }
+//     return propParts;
+// }
+
+// export default splitDottedPath;
+
+// //////////////////// PARSER V2 /////////////////////////
+
+const escapeChar = '\\';
+const dotChar = '.';
+
+function parseDotPath(path: string): string[] {
+    const result = [] as string[];
     let currentPart = '';
+
     const savePart = () => {
-        if (currentPart.length > 0 && ![...currentPart].every(char => char === ' ')) {
-            propParts.push(currentPart);
-            currentPart = '';
-        }
+        if (currentPart.length <= 0) return;
+
+        result.push(currentPart);
+        currentPart = '';
     };
-    for (let i = 0; i < dottedPath.length; i++) {
-        const currentChar = dottedPath[i];
-        const nextChar = dottedPath[i + 1];
+
+    for (let i = 0; i < path.length; i++) {
+        const currentChar = path[i];
+        const nextChar = path[i + 1];
+
         if (currentChar === escapeChar && (nextChar === escapeChar || nextChar === dotChar)) {
             currentPart += nextChar;
-            i += 1;
-            continue;
-        }
-        if (!(currentChar === dotChar)) {
-            currentPart += currentChar;
-            if (i !== dottedPath.length - 1) {
-                continue;
-            }
-        }
-        savePart();
+            i++;
+        } else if (currentChar === dotChar) savePart(); else currentPart += currentChar;
     }
-    for (let i = 0; i < propParts.length; i++) {
-        propParts[i] = propParts[i].replace('\\\\', '\\');
-    }
-    return propParts;
+
+    savePart();
+
+    return result;
 }
 
-export default splitDottedPath;
+export default parseDotPath;
