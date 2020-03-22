7b351c5b83a3cb1404f4c6b8f564c8e0be0b7aaa
2CHEVSKII <prsroman3@gmail.com>
2020-03-22 14:28:41 +0300

    parserv2 tests

--- tests/helpers/parser.test.js
+++ tests/helpers/parser.test.js
@@ -1,10 +1,17 @@
 import { describe, it } from 'mocha';
-import guess from 'assume';
-import assert from 'assert';
-import parseDots from '../../src/helpers/parser';
+import assume from 'assume';
+import parse from '../../src/helpers/parser';
 
 describe('Parser tests:', function () {
     it("- Parse simple path :: 'foo.bar.baz.42' -> [ foo, bar, baz, 42 ]", function () {
+        assume(parse('foo.bar.baz.42')).eql(['foo', 'bar', 'baz', '42']);
+    });
 
+    it("- Parse escaped path :: 'foo\\.bar.\\\\.baz\\.\\..42' -> [ foo.bar, \\.baz.., 42]", function () {
+        assume(parse('foo\\.bar.\\\\.baz\\.\\..42')).eql(['foo.bar', '\\.baz..', '42']);
+    });
+
+    it("- Parse unusual path :: '   .foo. .bar..\\..baz' -> [ '   ', foo, ' ', bar, '', '', '', baz ]", function () {
+        assume(parse('   .foo. .bar..\\..baz')).eql(['   ', 'foo', ' ', 'bar', '', '.', 'baz']);
     });
 });
