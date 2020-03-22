c52804f5df72052912d720dc7524359b6508c5ef
2CHEVSKII <prsroman3@gmail.com>
2020-03-22 03:40:28 +0300

    temp

--- tests/helpers/parser.test.js
+++ tests/helpers/parser.test.js
@@ -1,29 +1,10 @@
 import { describe, it } from 'mocha';
-import assume from 'assume';
-import parse from '../../src/helpers/parser';
+import guess from 'assume';
+import assert from 'assert';
+import parseDots from '../../src/helpers/parser';
 
 describe('Parser tests:', function () {
-    it('- Properly parse simple path', function () {
-        const parseResult = parse('part1.part2.part3');
-        assume(parseResult).length(3);
-        assume(parseResult).eql(['part1', 'part2', 'part3']);
-    });
+    it("- Parse simple path :: 'foo.bar.baz.42' -> [ foo, bar, baz, 42 ]", function () {
 
-    it('- Properly parse escaped path', function () {
-        const parseResult = parse('part1\\.part2.part3');
-        assume(parseResult).length(2);
-        assume(parseResult).eql(['part1.part2', 'part3']);
-    });
-
-    it('- Properly parse partially incorrect paths', function () {
-        const parseResult = parse('part1..part2');
-        assume(parseResult).length(2);
-        assume(parseResult).eql(['part1', 'part2']);
-        assume(parse('..part1')).not.empty();
-    });
-
-    it('- Return empty on incorrect paths', function () {
-        assume(parse('....')).empty();
-        assume(parse('  ')).empty();
     });
 });
