31774158e9ac135abb8eca205819eb2a09e62438
2CHEVSKII <prsroman3@gmail.com>
2020-03-24 02:10:19 +0300

    Export values and make tests for them

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import core from './core/accessor';
+import parseDotPath from './helpers/parser';
+
+export default { ...core, parseDotPath };
---
+++ tests/index.test.ts
@@ -0,0 +1,18 @@
+import { describe, it } from 'mocha';
+import assume from 'assume';
+import dotnot from '../src/index';
+
+describe('Common tests of exports:', function () {
+    it('- Exported object must be an... object...', function () {
+        assume(dotnot).is.an('object');
+    });
+
+    it('- Exported object must contain a set of functions', function () {
+        assume(dotnot).contains('getValue');
+        assume(dotnot).contains('setValue');
+        assume(dotnot).contains('hasValue');
+        assume(dotnot).contains('copyProperty');
+        assume(dotnot).contains('moveProperty');
+        assume(dotnot).contains('parseDotPath');
+    });
+});
